b21ebe7b69ea9099fdaa1207b0d2072a4b8e4acb
aayushigoel07 <agoel7@rockets.utoledo.edu>
2025-11-05 16:09:40 -0500

    Updated home screen UI and added background video

--- src/components/Homescreen/Homescreen.jsx
+++ src/components/Homescreen/Homescreen.jsx
@@ -3,6 +3,9 @@ import './Homescreen.css'
 function Homescreen() {
   return (
     <div id="home" className="homescreen">
+      <video className="bg-video" autoPlay muted loop playsInline>
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
       <h1 className="homescreen-text">Learn. Innovate. Build.</h1>
     </div>
   )
